a32bca9581f792503818692e243dbd9efc8d83e2
Pavel Sysoev <pashgan22@yandex.ru>
2022-02-25 22:36:50 +0700

    Добавление в css media-запросов. Рефакторинг JS.

--- js/app.js
+++ js/app.js
@@ -1,124 +1,117 @@
-// начальная инициализация календаря
-function init(selectedDate) {
-  // количество дней в неделе
-  const countOfDaysInWeek = 7
+// IIFE
+(function () {
 
-  // текуая дата
-  const currentDate = new Date()
+  // начальная инициализация календаря
+  function init(selectedDate) {
+    // количество дней в неделе
+    const countOfDaysInWeek = 7;
 
-  // выделяем из полученный даты месяц и год
-  const optionsOfDate = {
-    year: 'numeric',
-    month: 'long'
-  }
+    // текуая дата
+    const currentDate = new Date();
 
-  let date = selectedDate.toLocaleString('ru', optionsOfDate)
+    // выделяем из полученный даты месяц и год
+    const optionsOfDate = {
+      year: 'numeric',
+      month: 'long'
+    };
 
-// убираем 'г.'
-  date = date.substring(0, date.indexOf(' г.'))
+    const date = selectedDate.toLocaleString('ru-RU', optionsOfDate);
 
-// находим ноду для вывода текущего месяца и выводим
-  document.querySelector('.monthYear').innerText = date
+    // находим ноду для вывода текущего месяца и выводим
+    document.querySelector('.monthYear').innerText = date.replace(' г.', '');
 
-// Функция получения количества дней в месяце
-  Date.prototype.daysInMonth = function () {
-    return 33 - new Date(this.getFullYear(), this.getMonth(), 33).getDate();
-  };
+    // определяем количество дней в текущем месяце
+    const numberOfDaysInMonth = 33 - new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 33).getDate();
 
-// определяем количество дней в текущем месяце
-  const numberOfDaysInMonth = selectedDate.daysInMonth()
+    // формируем дату - первый день текущего месяца
+    const firstDayOfCurrentMonthInEnWeek = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1).getDay() - 1;
+    const firstDayOfCurrentMonthInRuWeek = firstDayOfCurrentMonthInEnWeek < 0 ? 6 : firstDayOfCurrentMonthInEnWeek;
 
-// формируем дату - первый день текущего месяца
-  let firstDayOfCurrentMonth = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), 1).getDay() - 1
-  firstDayOfCurrentMonth = firstDayOfCurrentMonth < 0 ? 6 : firstDayOfCurrentMonth
+    // таблица для формирования дней месяца
+    const daysOfMonth = Array(42).fill(0);
 
-// таблица для формирования дней месяца
-  const daysOfMonth = Array(42).fill(0)
-
-// формируем данные для таблицы дней месяца
-  for (let i = firstDayOfCurrentMonth; i - firstDayOfCurrentMonth < numberOfDaysInMonth; i++) {
-    daysOfMonth[i] = i - firstDayOfCurrentMonth + 1
-  }
-
-// считаем количество строк(недель) для формирования таблицы дней
-  const intermediateDataForDaysOfMonthTable = firstDayOfCurrentMonth + numberOfDaysInMonth
-  let countOfWeeks = ~~((intermediateDataForDaysOfMonthTable) / countOfDaysInWeek)
-
-  if (intermediateDataForDaysOfMonthTable % countOfDaysInWeek) {
-    countOfWeeks++
-  }
-
-// "отрезаем" бесполезные ячейки
-  daysOfMonth.length = countOfWeeks * countOfDaysInWeek
-
-// находим ноду тела таблицы
-  const tbodyNode = document.querySelector('table tbody')
-
-// заполняем дни в таблице
-  let tr
-  daysOfMonth.forEach((item, step) => {
-    step++
-
-    // для каждой недели формируем строку
-    if (step === 1 || (step % countOfDaysInWeek) - 1 === 0) {
-      tr = document.createElement('tr')
-      tbodyNode.appendChild(tr)
+    // формируем данные для таблицы дней месяца
+    for (let i = firstDayOfCurrentMonthInRuWeek; i - firstDayOfCurrentMonthInRuWeek < numberOfDaysInMonth; i++) {
+      daysOfMonth[i] = i - firstDayOfCurrentMonthInRuWeek + 1;
     }
 
-    // формируем ячейку
-    let td = document.createElement('td')
+    // считаем количество строк(недель) для формирования таблицы дней
+    const intermediateDataForDaysOfMonthTable = firstDayOfCurrentMonthInRuWeek + numberOfDaysInMonth;
+    let countOfWeeks = Math.floor(intermediateDataForDaysOfMonthTable / countOfDaysInWeek);
 
-    if (item === 0) {
-      td.innerText = ''
-      td.className = 'empty'
-    } else {
-      td.innerText = item
+    if (intermediateDataForDaysOfMonthTable % countOfDaysInWeek) {
+      countOfWeeks++;
+    }
 
-      if (item === currentDate.getDate() && selectedDate.getMonth() === currentDate.getMonth()) {
-        td.className = 'today'
+    // "отрезаем" бесполезные ячейки
+    daysOfMonth.length = countOfWeeks * countOfDaysInWeek;
+
+    // находим ноду тела таблицы
+    const tbodyNode = document.querySelector('table tbody');
+
+    // заполняем дни в таблице
+    let tr;
+    daysOfMonth.forEach((item, i) => {
+      const step = i + 1;
+
+      // для каждой недели формируем строку
+      if (step === 1 || (step % countOfDaysInWeek) - 1 === 0) {
+        tr = document.createElement('tr');
+        tbodyNode.appendChild(tr);
       }
+
+      // формируем ячейку
+      const td = document.createElement('td');
+
+      if (item === 0) {
+        td.innerText = '';
+        td.className = 'empty';
+      } else {
+        td.innerText = item;
+
+        if (item === currentDate.getDate() && selectedDate.getMonth() === currentDate.getMonth()) {
+          td.className = 'today';
+        }
+      }
+
+      tr.appendChild(td);
+    })
+  }
+
+  // -------------------------------------------------------------------------
+
+  // очистка таблицы дней
+  function clearTable() {
+    const node = document.querySelector('table');
+
+    // rows[0] это заголовок - оставляем
+    while (node.rows[1]) {
+      node.deleteRow(1);
+    }
+  }
+
+  // получаем текущую дату
+  let selectedDate = new Date();
+
+  // инициализация календаря
+  init(selectedDate);
+
+  // установка нового периода календаря (предыдущий/следующий)
+  function setNewCalendarPeriod(event) {
+    if (event.target.className === 'left button') {
+      selectedDate = new Date(selectedDate.getFullYear(), selectedDate.getMonth() - 1, 1);
+    } else if (event.target.className === 'right button') {
+      selectedDate = new Date(selectedDate.getFullYear(), selectedDate.getMonth() + 1, 1);
     }
 
-    tr.appendChild(td)
-  })
-}
+    clearTable();
 
-// -------------------------------------------------------------------------
-
-// очистка таблицы дней
-function clearTable() {
-  const node = document.querySelector('table');
-
-  // rows[0] это заголовок - оставляем
-  while(node.rows[1]) {
-    node.deleteRow(1);
+    init(selectedDate);
   }
-}
 
-// получаем текущую дату
-let selectedDate = new Date()
+  const leftButton = document.querySelector('.left');
+  leftButton.addEventListener('click', setNewCalendarPeriod);
 
-// Дата для отладки
-// let selectedDate = new Date(2022, 8, 5)
-
-// инициализация календаря
-init(selectedDate)
-
-// переход на предыдущий месяц
-function prev() {
-  selectedDate = new Date(selectedDate.getFullYear(), selectedDate.getMonth() - 1, 1)
-
-  clearTable()
-
-  init(selectedDate)
-}
-
-// переход на следующий месяц
-function next() {
-  selectedDate = new Date(selectedDate.getFullYear(), selectedDate.getMonth() + 1, 1)
-
-  clearTable()
-
-  init(selectedDate)
-}
-
+  const rightButton = document.querySelector('.right');
+  rightButton.addEventListener('click', setNewCalendarPeriod);
+})()
