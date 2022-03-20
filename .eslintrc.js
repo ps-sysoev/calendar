0a109d621f8ac578b2574974f6f1bf8a39babe52
Alex Kalinin <alexkalinin.ru@gmail.com>
2022-03-20 13:26:36 +0700

    Eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,12 @@ module.exports = {
   // parser: 'babel-eslint',
   extends: [
     'eslint:recommended',
+    "airbnb",
+    "airbnb/hooks"
   ],
   rules: {
     'import/prefer-default-export': 1,
+    'linebreak-style': 0,
   },
   env: {
     browser: true,
